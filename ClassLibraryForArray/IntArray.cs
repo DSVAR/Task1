6fcb63c280ec01969567b35171c08f13180b29a3
DVAR <danilka070103@gmail.com>
2020-11-26 05:57:09 +0300

    Добавьте файлы проекта.

---
+++ ClassLibraryForArray/IntArray.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryForArray
+{
+    class IntArray
+    {
+       private int[] a;    //закрытый одномерный массив
+       private int length;      //закрытая длина
+
+        public int Length { get; set; }
+        public int this[int i]
+        {
+            get { return 0; }
+            set { a[i] = value; }
+        }
+        public IntArray(int length)
+        {
+
+        }
+        public IntArray(params int[] arr)
+        {
+
+        }
+       
+
+        public static IntArray RandomIntArray(int length, int a, int b)
+        {
+
+            return null;
+        }
+
+        public static int SumArray(IntArray arr)
+        {
+           
+            return 1;
+        }
+
+
+        public static IntArray operator ++(IntArray arr)
+        {
+            IntArray intArray = new IntArray(arr.a);
+
+            return intArray;
+
+        }
+        public static IntArray operator +(IntArray x, int y)
+        {
+            IntArray intArray =new IntArray()
+        }
+    }
+}
