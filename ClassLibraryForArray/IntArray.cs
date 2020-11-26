2fbb2ff7cc4faeb067732314166b82317e0e0fb3
Nyrmehametov danil <danilka468@outlook.com>
2020-11-26 18:01:33 +0300

    main

--- ClassLibraryForArray/IntArray.cs
+++ ClassLibraryForArray/IntArray.cs
@@ -14,8 +14,14 @@ namespace ClassLibraryForArray
         public int Length { get; set; }
         public int this[int i]
         {
-            get { return 0; }
-            set { a[i] = value; }
+            get {
+                if (i >= 0 && i < length) return a[i];
+                else throw new IndexOutOfRangeException();
+            }
+            set {
+                if (i >= 0 && i < length) a[i] = value;
+                else throw new IndexOutOfRangeException();
+            }
         }
         public IntArray(int length)
         {
@@ -42,14 +48,82 @@ namespace ClassLibraryForArray
 
         public static IntArray operator ++(IntArray arr)
         {
-            IntArray intArray = new IntArray(arr.a);
-
+            IntArray intArray = new IntArray(arr.length);
+            for (int i = 0; i < arr.length; i++)
+            {
+                intArray[i] = arr.a[i]++;
+            }
             return intArray;
 
         }
         public static IntArray operator +(IntArray x, int y)
         {
-            IntArray intArray =new IntArray()
+            IntArray temp = new IntArray(x.length);
+            for(int i = 0; i < x.length; i++)
+                temp[i] =x[i]+ y;
+            
+            return temp;
         }
+
+        public static IntArray operator +(int x, IntArray y)
+        {
+            IntArray temp = new IntArray(y.length);
+            for (int i = 0; i < y.length; i++)
+                temp[i] = x + y[i];
+
+            return temp;
+        }
+
+        public static IntArray operator +(IntArray x, IntArray y)
+        {
+            int len = x.length < y.length ? x.length : y.length;
+            IntArray temp = new IntArray(len);
+            for (int i = 0; i < len; i++) temp[i] = x[i] + y[i];
+
+            return temp;
+        }
+
+        /////////////////////////
+        ///
+        //1
+        public static IntArray operator --(IntArray arr)
+        {
+            IntArray intArray = new IntArray(arr.length);
+            for (int i = 0; i < arr.length; i++)
+            {
+                intArray[i] = arr.a[i]--;
+            }
+            return intArray;
+
+        }
+        //2
+        public static IntArray operator -(IntArray x, int y)
+        {
+            IntArray temp = new IntArray(x.length);
+            for (int i = 0; i < x.length; i++)
+                temp[i] = x[i] - y;
+
+            return temp;
+        }
+        //3
+        public static IntArray operator -(int x, IntArray y)
+        {
+            IntArray temp = new IntArray(y.length);
+            for (int i = 0; i < y.length; i++)
+                temp[i] = x - y[i];
+
+            return temp;
+        }
+        //4
+        public static IntArray operator -(IntArray x, IntArray y)
+        {
+            int len = x.length < y.length ? x.length : y.length;
+            IntArray temp = new IntArray(len);
+            for (int i = 0; i < len; i++) temp[i] = x[i] - y[i];
+
+            return temp;
+        }
+
+
     }
 }
